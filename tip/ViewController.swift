676603f4645fcb828039dc87ff9aed657a95438a
Jashandeep Singh <jashanbhinder2@gmail.com>
2020-08-01 15:36:58 -0600

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Jashan Bhinder on 8/1/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
