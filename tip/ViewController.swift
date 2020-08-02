4fa524af6ddee48f345a240eac3487044cc3e5df
Jashandeep Singh <jashanbhinder2@gmail.com>
2020-08-02 16:00:04 -0600

    Adding the Logic code and User Interface

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // get the bill Amount
+        let bill = Double(billField.text!) ?? 0
+        
+        
+        
+        // Calculate the tip and total
+        let tipPercentages = [0.15, 0.2, 0.22]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        // Update the tip and total labels
+            
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
